ff105e0f97565b1717da1232668dd1d5314c9d44
杨红磊 <yanghl01@beantechs.cn>
2020-07-09 21:15:41 +0800

    init

---
+++ Example/YHLCore/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  YHLCore
+//
+//  Created by 272789124@qq.com on 07/09/2020.
+//  Copyright (c) 2020 272789124@qq.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
